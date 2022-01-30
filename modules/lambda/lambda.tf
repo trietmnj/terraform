642cc779e1e6a454c42bed772512c54548ed450d
TMNT <trietpersonal@gmail.com>
2022-01-30 10:20:45 -0500

    added lambda s3 actions + ec2 network interface

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -5,7 +5,7 @@ variable "environment" {}
 variable "bucket_id" {}
 
 resource "aws_iam_role" "role" {
-  name               = "${var.project_name}-${var.environment}_lambda_role"
+  name               = "${var.project_name}_${var.environment}_lambda_role"
   assume_role_policy = <<EOF
   {
     "Version": "2012-10-17",
@@ -24,13 +24,14 @@ resource "aws_iam_role" "role" {
 }
 
 resource "aws_iam_role_policy" "policy" {
-  name   = "${var.project_name}-${var.environment}_lambda_general_policy"
+  name   = "${var.project_name}_${var.environment}_lambda_general_policy"
   role   = aws_iam_role.role.id
   policy = <<EOF
   {
     "Version": "2012-10-17",
     "Statement": [
       {
+        "Effect": "Allow",
         "Action": [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
@@ -45,6 +46,31 @@ resource "aws_iam_role_policy" "policy" {
           "s3:ListAllMyBuckets"
         ],
         "Resource": "arn:aws:s3:::"
+      },
+      {
+        "Effect": "Allow",
+        "Action": ["s3:ListBucket"],
+        "Resource": ["arn:aws:s3:::${var.bucket_id}"]
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject"
+        ],
+        "Resource": ["arn:aws:s3:::${var.bucket_id}/*"]
+      },
+      {
+         "Effect":"Allow",
+         "Action":[
+            "ec2:CreateNetworkInterface",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DeleteNetworkInterface",
+            "ec2:DescribeTags",
+            "ec2:DescribeInstances"
+          ],
+         "Resource":"*"
       }
     ]
   }
